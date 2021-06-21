09801daa7fbf17e709c6da205a816671ed9f130b
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-06-21 08:55:26 -0300

    Testando operações

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,15 +1,22 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
-	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Ana Maria", "ana@gmail.com");
+	public static void main(String[] args) {		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		Pessoa p = em.find(Pessoa.class, 2);
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		System.out.println(p);
+		
+		System.out.println("Pronto");
+		em.close();
+		emf.close();
 	}
 }
